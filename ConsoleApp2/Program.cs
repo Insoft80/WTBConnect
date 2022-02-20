b2e8b595778884862af74e224b82775343aafd3c
Ingo Hartmann <ingo@ingohartmann.de>
2022-02-20 13:39:09 +0100

    PreRelease

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -14,25 +14,65 @@ namespace WTBConnect
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("WTB Connect V0.9");
             SerialPort serialPort = new SerialPort();
-            serialPort.PortName = "COM8";
-            serialPort.Open();
+            string PortName = GetPortName();
+            if (!string.IsNullOrEmpty(PortName))
+            {
+                Console.WriteLine($"Connecting WTB device on {PortName}");
+                serialPort.PortName = PortName;
+           
+                serialPort.Open();
             
-            string Message = serialPort.ReadLine();
-            Console.WriteLine(Message);
-            serialPort.Close();
+                string Message = serialPort.ReadLine();
+                Console.WriteLine(Message);
+                serialPort.Close();
 
+           
+            
+            }
+            //Anzeige(pnpGeräte); Console.ReadLine();
+        }
+
+        private static string GetPortName()
+            // Get name of USB port where Arduion is connected via CH340 chip
+        {
+            string portName = null;
             var usbController = USBControllerDevice.GetInstances().Cast<USBControllerDevice>();
             var pnpGeräte = PnPEntity.GetInstances().Cast<PnPEntity>().
               Where(p => usbController.Any(uc => uc.Dependent.RelativePath.
                 Split('\"')[1].Replace(@"\\", @"\") == p.DeviceID) && p.Status == "OK");
-            foreach (string s in SerialPort.GetPortNames())
+            Boolean found = false;
+            Console.WriteLine("Detecting connected PNP Entities ...");
+            foreach (var gerät in pnpGeräte)
             {
-                Console.WriteLine("    {0}",s);
-            }
-            Anzeige(pnpGeräte); Console.ReadLine();
-        }
+                Console.WriteLine("    " + gerät.Caption + "    DeviceID: " + gerät.DeviceID);
+                if (gerät.Caption.Contains("CH340"))
+                {
+                    Console.Write("        found - trying to detect PortName ... ");
+                    int pos1 = gerät.Caption.LastIndexOf("COM");
+                    if (pos1 > 0)
+                    {
+                        int pos2 = gerät.Caption.IndexOf(")", pos1);
+                        if (pos2 > pos1)
+                        {
+                            portName = gerät.Caption.Substring(pos1, pos2 - pos1);
+                            found = true;
+                            Console.WriteLine(portName + "!");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"ERROR - No COM-Port found ({pos1}/{pos2})");
+                        }
+                    }
+                  
 
+                }
+            }
+            if (!found)
+                Console.WriteLine("ERROR detecting WTB device - not connected or no port information found!");
+            return(portName);
+        }
         private static void Anzeige(IEnumerable<PnPEntity> pnpGeräte)
         {
             foreach (var gerät in pnpGeräte)
